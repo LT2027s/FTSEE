f9a707b26f56eac36ccbcf84e02476703d97928e
LT2027s <ltimms@ash.nl>
2026-09-14 12:53:15 +0200

    Bug fixed Stable Checkpoint

--- ftcscout.js
+++ ftcscout.js
@@ -43,5 +43,5 @@ export async function getTeamStats(teamNumber, season) {
   });
 
   const result = await response.json();
-  return result.data.teamByNumber;
+  return result.data.teamByNumber.quickStats;
 }
