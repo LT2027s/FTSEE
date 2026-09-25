cb2b7f38d11a89b95ce34c3514e2aa7a6d120140
LT2027s <ltimms@ash.nl>
2026-09-25 10:43:49 +0200

    Final final

--- firebase.js
+++ firebase.js
@@ -1,14 +1,17 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
-import { getFirestore, collection, query, where, getDocs, doc, deleteDoc } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
+import { getFirestore, collection, query, where, getDocs, doc, deleteDoc } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-firestore.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDhe_m3wB5CyDktGJcwQZP7kmIFjZrDx2k",
-  authDomain: "ftsee-777.firebaseapp.com",
-  projectId: "ftsee-777",
-  storageBucket: "ftsee-777.firebasestorage.app",
-  messagingSenderId: "812545382222",
-  appId: "1:812545382222:web:fa171bc9bd6003244d301f"
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyDhe_m3wB5CyDktGJcwQZP7kmIFjZrDx2k",
+    authDomain: "ftsee-777.firebaseapp.com",
+    databaseURL: "https://ftsee-777-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "ftsee-777",
+    storageBucket: "ftsee-777.firebasestorage.app",
+    messagingSenderId: "812545382222",
+    appId: "1:812545382222:web:4e96e485f85c3caa4d301f",
+    measurementId: "G-DH34RP890M"
+  };
+
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
