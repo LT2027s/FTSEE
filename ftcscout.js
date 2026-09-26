c2649b921c021c2fd8d9b27636f1467a0fd0fb17
LT2027s <ltimms@ash.nl>
2026-09-26 16:23:40 +0200

    Comments

--- ftcscout.js
+++ ftcscout.js
@@ -1,8 +1,14 @@
+//function getTeamBasics which uses team number
 export async function getTeamBasics(teamNumber) {
+  //Waits until connected with API and that the API recieves the all of the data
   const response = await fetch('https://api.ftcscout.org/graphql', {
+    //Method POST to send the GrahpQL query
     method: 'POST',
+    //tells server that the query is formatted in JSON
     headers: { 'Content-Type': 'application/json' },
+    //turn the query into a JSON string for the HTTP
     body: JSON.stringify({
+      //the query that is sent to FTCscout API
       query: `
         query GetTeam($number: Int!) {
           teamByNumber(number: $number) {
@@ -17,15 +23,23 @@ export async function getTeamBasics(teamNumber) {
       variables: { number: teamNumber}
     })
   });
-
+//Waits till data from API processed and sent back
   const result = await response.json();
+  //Gets the return values. If value doesnt exist then set it to null
   return result.data?.teamByNumber?? null;
 }
+
+//function getTeamStats which uses team number and season
 export async function getTeamStats(teamNumber, season) {
+  //Waits until connected with the API recieves the data
   const response = await fetch('https://api.ftcscout.org/graphql', {
+        //Method POST to send the GrahpQL query
     method: 'POST',
+        //tells server that the query is formatted in JSON
     headers: { 'Content-Type': 'application/json' },
+     //turn the query into a JSON string for the HTTP
     body: JSON.stringify({
+            //the query that is sent to FTCscout API
       query: `
         query GetTeam($number: Int!, $season: Int!) {
           teamByNumber(number: $number) {
@@ -41,7 +55,8 @@ export async function getTeamStats(teamNumber, season) {
       variables: { number: teamNumber, season: season }
     })
   });
-
+//Waits till data from API processed and sent back
   const result = await response.json();
+    //Gets the return values. If value doesnt exist then set it to null
   return result.data?.teamByNumber?.quickStats?? null;
 }
